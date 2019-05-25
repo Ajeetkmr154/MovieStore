1a06bcab05250137d172a8afdeccac6a0ec73568
Ajeet Kumar <Ajeetkmr154@gmail.com>
2019-05-25 22:12:20 +0530

    Movie Name modules developed

---
+++ MovieStore/Controllers/MovieController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MovieStore.Models;
+
+namespace MovieStore.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie/Random
+        public ActionResult Random()
+        {
+            var Movie = new Movie() { Name="Ajeet Kumar"};
+            return View(Movie);
+        }
+    }
+}
